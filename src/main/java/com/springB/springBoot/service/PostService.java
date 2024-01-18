1eb60d2a791f0d6dd963a25951fb54ebd40a6ffb
hg_96 <hg_96@naver.com>
2024-01-18 23:21:15 +0900

    전체 조회

--- src/main/java/com/springB/springBoot/service/PostService.java
+++ src/main/java/com/springB/springBoot/service/PostService.java
@@ -2,7 +2,11 @@ package com.springB.springBoot.service;
 
 import com.springB.springBoot.payload.PostDto;
 
+import java.util.List;
+
 public interface PostService {
     PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
 
 }
--- src/main/java/com/springB/springBoot/service/impl/PostServiceImpl.java
+++ src/main/java/com/springB/springBoot/service/impl/PostServiceImpl.java
@@ -6,6 +6,9 @@ import com.springB.springBoot.repository.PostRepository;
 import com.springB.springBoot.service.PostService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PostServiceImpl implements PostService {
 
@@ -17,20 +20,36 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDto createPost(PostDto postDto) {
+        Post post = mapToEntity(postDto);
+        Post newPost = postRepository.save(post);
+        PostDto postResponse = mapToDTO(newPost);
 
+        return postResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
+    }
+
+    private PostDto mapToDTO(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+
+        return  postDto;
+    }
+
+    private Post mapToEntity(PostDto postDto) {
         Post post = new Post();
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
         post.setContent(postDto.getContent());
-
-        Post newPost = postRepository.save(post);
-
-        PostDto postResponse = new PostDto();
-        postResponse.setId(newPost.getId());
-        postResponse.setTitle(newPost.getTitle());
-        postResponse.setDescription(newPost.getDescription());
-        postResponse.setContent(newPost.getContent());
-
-        return postResponse;
+        return post;
     }
+
 }
